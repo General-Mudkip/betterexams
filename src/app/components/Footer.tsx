74815c53865b6249f058d89bbfb4e6f6f1a508b9
General-Mudkip <bencethegreat@gmail.com>
2023-07-04 00:37:56 +0100

    Better footer positioning

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,29 +1,32 @@
 function Footer() {
-    return(
-        <footer>
-            <div className="flex absolute left-0 bottom-24 w-full justify-center">
-                <a href="/" className="group text-blue-400 hover:text-blue-500 transition-all duration-300">
-                    About
-                    <span className="block max-w-0 bg-blue-400 group-hover:max-w-full transition-all duration-300 h-0.5 group-hover:bg-blue-500"></span>
-                </a>
-                
-                &nbsp;&nbsp;•&nbsp;&nbsp;
+  return (
+    <footer className="flex flex-col items-center">
+      <div className="flex w-full flex-row flex-wrap items-center justify-center space-x-2 text-center">
+        <a href="/" className="group text-blue-400 hover:text-blue-500 transition-all duration-300">
+          About
+          <span className="block max-w-0 bg-blue-400 group-hover:max-w-full transition-all duration-300 h-0.5 group-hover:bg-blue-500"></span>
+        </a>
 
-                <a href="/" className="group text-red-400 hover:text-red-500">
-                    Contact
-                    <span className="block max-w-0 bg-red-400 group-hover:max-w-full transition-all duration-300 h-0.5 group-hover:bg-red-500"></span>
-                </a>
-                
-                &nbsp;&nbsp;<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">•</a> {/*  teehee */} &nbsp;&nbsp;
-                
-                <a href="https://github.com/General-Mudkip/exam-help-tests" target="_blank" className="group text-green-400 hover:text-green-500">
-                    Github
-                    <span className="block max-w-0 bg-green-400 group-hover:max-w-full transition-all duration-300 h-0.5 group-hover:bg-green-500"></span>
-                </a>
-            </div>
-        </footer>
+        <p>•</p>
+
+        <a href="/" className="group text-red-400 hover:text-red-500">
+          Contact
+          <span className="block max-w-0 bg-red-400 group-hover:max-w-full transition-all duration-300 h-0.5 group-hover:bg-red-500"></span>
+        </a>
+
         
-    )
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="text-white hover:text-red-500 transition-all duration-300">
+          •
+        </a>
+        {/*  teehee */}
+
+        <a href="https://github.com/General-Mudkip/exam-help-tests" target="_blank" className="group text-green-400 hover:text-green-500">
+          Github
+          <span className="block max-w-0 bg-green-400 group-hover:max-w-full transition-all duration-300 h-0.5 group-hover:bg-green-500"></span>
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,17 @@ import ChoicesForm from './components/Form';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-16 p-24">
+    <main className="min-h-screen h-full flex flex-col items-center gap-16 p-24">
       <h1 className="text-6xl font-bold text-center">Better Exams</h1>
 
       <div className="text-slate-400 items-center h-full">
         <ChoicesForm />
       </div>
+      
+      <div className="flex-grow" />
 
       <Footer />
+      
     </main>
   )
 }
