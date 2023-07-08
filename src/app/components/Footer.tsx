b95336d5c804a66532a72b9454b032ab34fb21fe
General-Mudkip <bencethegreat@gmail.com>
2023-07-08 20:21:39 +0100

    Fixed Google being mad about SEO

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -10,23 +10,23 @@ function Footer() {
     return (
         <footer className="flex flex-col items-center w-96 mt-10 lg:mt-0">
             <div className="flex w-full flex-row flex-wrap items-center justify-center space-x-2 text-center">
-                <a
+                <button
                     onClick={() => setAboutIsOpen(true)}
-                    className="group cursor-pointer text-blue-400 hover:text-blue-500 transition-all duration-300"
+                    className="group cursor-pointer !bg-transparent text-blue-400 hover:text-blue-500 transition-all duration-300"
                 >
                     About
                     <span className="block max-w-0 bg-blue-400 group-hover:max-w-full transition-all duration-300 h-0.5 group-hover:bg-blue-500"></span>
-                </a>
+                </button>
 
                 <p>•</p>
 
-                <a
+                <button
                     onClick={() => setContactIsOpen(true)}
-                    className="group cursor-pointer text-red-400 hover:text-red-500"
+                    className="group cursor-pointer !bg-transparent text-red-400 hover:text-red-500"
                 >
                     Contact
                     <span className="block max-w-0 bg-red-400 group-hover:max-w-full transition-all duration-300 h-0.5 group-hover:bg-red-500"></span>
-                </a>
+                </button>
 
                 <a
                     href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
