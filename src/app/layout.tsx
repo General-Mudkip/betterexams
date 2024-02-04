4e05adc553011762cdf57c2fd25f283817225410
General-Mudkip <bencethegreat@gmail.com>
2024-02-04 18:06:15 +0000

    Open Graph previews

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,31 @@
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Better Exams | Past Papers Made Easy",
   keywords: "examinations.ie, exams, leaving cert, junior cert, past papers",
-  description: "The best way to search for past leaving cert exam papers and marking schemes sourced from examinations.ie's material archive.",
+  description:
+    "The best way to search for past leaving cert exam papers and marking schemes sourced from examinations.ie's material archive.",
+  openGraph: {
+    title: "Better Exams | Past Papers Made Easy",
+    description:
+      "The best way to search for past leaving cert exam papers and marking schemes sourced from examinations.ie's material archive.",
+    url: "https://betterexams.ie",
+    type: "website",
+    siteName: "Better Exams",
+    images: [
+      {
+        url: "https://betterexams.ie/better_exams_preview.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Better Exams | Past Papers Made Easy",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
